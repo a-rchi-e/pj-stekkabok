192c195d9a2fce26d8381cffd651acbe66b0d8f7
zwergpirat <zwerg0pirat@gmail.com>
2025-03-13 12:55:33 +0100

    frontend: more testing

--- client/src/tests/helper_funcs.test.tsx
+++ client/src/tests/helper_funcs.test.tsx
@@ -4,13 +4,13 @@ const fetchMocker = createFetchMock(vi);
 fetchMocker.enableMocks();
 
 import { describe, it, expect } from 'vitest'
-import { render, screen } from '@testing-library/react';
-import { getAllRooms } from '../helper_funcs';
+import { screen } from '@testing-library/react';
+import { getAllRooms, getAvailableRooms, requestCheckout } from '../helper_funcs';
 
 describe('testing get all rooms api', () => {
-    // beforeEach(() => {
-    //     fetch.resetMocks();
-    //   });
+    beforeEach(() => {
+        fetchMock.resetMocks();
+      });
     it('should render all rooms from server', async () => {
         fetchMock.mockResponseOnce(JSON.stringify({ name: 'bob', prod_id: '123', imgPath:'testimg', price:999, desc:'lol', sleeps: 13, days:13 }));
         const result = await getAllRooms();
